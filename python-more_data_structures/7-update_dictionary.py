29eb1220485fbeaedda9cdd22773063cfce4915e
MarioMelengu <10030@holbertonstudents.com>
2025-01-23 11:47:44 +0000

    new file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-def print_sorted_dictionary(a_dictionary):
-    a_dictionary = dict(sorted(a_dictionary.items()))
-    for key, value in a_dictionary.items():
-        print("{}: {}".format(key, value))
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
