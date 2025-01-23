d8f8e3d8fd4c02c7910efb6fc248981a50fe108b
MarioMelengu <10030@holbertonstudents.com>
2025-01-23 13:16:36 +0000

    new file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    nr_elem = 0
+    en_list = 0
     for i in range(x):
         try:
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
-                nr_elem += 1
+                en_list += 1
             else:
                 continue
         except IndexError:
             raise IndexError
             break
     print()
-    return nr_elem
+    return en_list
