d47b4bf6dc159048220095c5fe053085fd214769
MarioMelengu <10030@holbertonstudents.com>
2025-01-29 11:08:13 +0000

    added files

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,8 +6,8 @@
 class Rectangle:
     """Class for a rectangle"""
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
