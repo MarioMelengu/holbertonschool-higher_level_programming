f4f8ae549bc2d53c82a5abb43c6cd91b7ff25cf0
MarioMelengu <10030@holbertonstudents.com>
2025-02-14 11:16:20 +0000

    added files

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,18 +1,39 @@
- #!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Convert CSV data to JSON format.
 
-import csv 
+This script reads data from a CSV file,
+converts it to JSON format, and writes the JSON data to `data.json`.
+
+Usage:
+    $ python3 task_02_csv.py
+
+Ensure that `data.csv` is in the same directory.
+"""
+
+
+import csv
 import json
 
+
 def convert_csv_to_json(csv_filename):
+    """
+    Convert CSV to JSON.
+
+    Args:
+        csv_filename (str): The name of the CSV file.
+
+    Returns:
+        bool: True if successful, False if file not found.
+    """
     try:
-        with open(csv_filename, mode="r", encoding="UTF8", newline="") as f:
-            csv_data = csv.DictReader(f)
-            data = list(csv_data)
-        return data
+        with open(csv_filename, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = [row for row in csv_reader]
 
-        with open("data.json", "w", encoding="UTF8") as file:
-            json.dump(data, file)
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
 
-    except Exception as e:
+        return True
+    except FileNotFoundError:
         return False
-    return True
